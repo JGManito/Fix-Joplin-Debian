c38e2d9e642dc869303f1323d82d25f3dd4d23e2
jonythunder <joaomanito1@gmail.com>
2020-05-01 03:14:10 +0100

    Create fix_joplin.sh
    Created script

---
+++ fix_joplin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Joplin is usually saved in "/home/$username/.joplin". If you for whatever reason have a different location, edit it here
+joplinPath = ~/.joplin
+
+#Extract AppImage
+./Joplin.AppImage --appimage-extract
+
+#Change owner of chrome-sandbox
+sudo chown root squashfs-root/chrome-sandbox
+
+#Change permission of chrome sandbox 
+sudo chmod 4755 squashfs-root/chrome-sandbox
+
+#Fix the desktop icon
+sed -i 's/.joplin\/.*\/".joplin\/squashfs-root\/joplin/g' ~/.local/share/applications/appimagekit-joplin.desktop
