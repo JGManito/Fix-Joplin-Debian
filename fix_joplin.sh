81b3192bfe9c6fdba1dc57914f9814eb99d45591
jonythunder <joaomanito1@gmail.com>
2020-05-01 03:34:06 +0100

    Update fix_joplin.sh
    Fixed errors in sintax and sed command

--- fix_joplin.sh
+++ fix_joplin.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #Joplin is usually saved in "/home/$username/.joplin". If you for whatever reason have a different location, edit it here
-joplinPath = ~/.joplin
+joplinPath=~/.joplin
+
+#Move to joplin directory
+pushd $joplinPath
 
 #Extract AppImage
 ./Joplin.AppImage --appimage-extract
@@ -13,4 +16,7 @@ sudo chown root squashfs-root/chrome-sandbox
 sudo chmod 4755 squashfs-root/chrome-sandbox
 
 #Fix the desktop icon
-sed -i 's/.joplin\/.*\/".joplin\/squashfs-root\/joplin/g' ~/.local/share/applications/appimagekit-joplin.desktop
+sed -i 's/.joplin\/.*/.joplin\/squashfs-root\/joplin/g' ~/.local/share/applications/appimagekit-joplin.desktop
+
+#Return to original working directory
+popd
